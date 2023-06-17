5254f7d9b527a9a380694b009af541ff753bf849
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-17 00:08:33 -0300

    fix: mobile banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
 
   return (
     <main className="xl:flex md:w-[88%]">
-      <section className="xl:w-[65%] p-8 lg:p-12 pt-16">
-        <h1 className="text-3xl lg:text-6xl font-bold">Find your perfect car</h1>
-        <div>
+      <section className="xl:w-[65%] lg:p-12 md:p-8 pt-16">
+        <div className='px-8 md:px-0'>
+          <h1 className="text-3xl lg:text-6xl font-bold">Find your perfect car</h1>
           <ul className="flex flex-wrap gap-4 mt-10 items-center">
             {carFilters.map((carFilter, index) => (
               <li key={index}>
@@ -24,7 +24,7 @@ export default function Home() {
             </li>
           </ul>
         </div>
-        <div className='h-[500px] md:h-[300px] mb-16 rounded-3xl mt-6 bg-[url("../../public/car-banner.jpg")] overflow-hidden bg-cover bg-center hover:scale-[1.02] duration-500'>
+        <div className='h-[500px] md:h-[300px] mb-16 md:rounded-3xl mt-6 bg-[url("../../public/car-banner.jpg")] overflow-hidden bg-cover bg-center hover:scale-[1.02] duration-500'>
           <div className='flex flex-col justify-center h-full w-full bg-gradient-to-r from-[rgba(0,0,0,.85)] lg:to-[transparent] to-[rgba(0,0,0,.35)] pl-7'>
             <h3 className='text-3xl font-bold'>Top Safety Pick</h3>
             <div className='mt-4'>
@@ -33,7 +33,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-4 mt-8 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-4 mt-8 w-full px-8 md:px-0">
           <SmallCard image="volvo-ex30.jpg" name='Volvo EX30' type='Electric' ratio={4.8} />
           <SmallCard image="volvo-xc90.jpg" name='Volvo XC90' type='Gasoline' ratio={4.7} />
           <SmallCard image="volvo-xc60.jpg" name='Volvo XC60' type='Electric' ratio={4.9} />
