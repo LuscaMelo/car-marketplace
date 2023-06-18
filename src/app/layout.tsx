6ebf1bbf9c2533bf553ab99278fc6ae5614164fd
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-17 21:41:39 -0300

    fix: bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
-"use client"
-
 import { Sidebar } from '@/components/Sidebar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { useState } from 'react'
-import FavoriteContext from '@/contexts/FavoriteContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
